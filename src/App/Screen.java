3409d5db0dbeab41b5a72e93c068303e451ac982
james <anasala.merthy@gmail.com>
2022-05-02 15:33:46 +0200

    Creation de la classe personne

---
+++ src/App/Screen.java
@@ -0,0 +1,15 @@
+package App;
+
+import javax.swing.*;
+
+public class Screen {
+    private JPanel panelRight;
+    private JPanel panelTop;
+    private JList ListPeople;
+    private JButton NewButton;
+    private JButton ExistingButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+}
