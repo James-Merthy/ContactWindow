5f45277750d942b418d3215c0734d6ead592f573
james <anasala.merthy@gmail.com>
2022-05-02 15:56:45 +0200

    Maj 3

--- src/App/Screen.java
+++ src/App/Screen.java
@@ -1,8 +1,9 @@
 package App;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Screen {
+public class Screen extends JFrame{
     private JPanel panelRight;
     private JPanel panelTop;
     private JList ListPeople;
@@ -12,4 +13,22 @@ public class Screen {
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
+    private JPanel panelMain;
+    private JPanel panelLeft;
+
+    public Screen(){
+
+        super("my Contact project");
+        this.setContentPane(this.panelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+
+        Screen screen = new Screen();
+
+        screen.setVisible(true);
+
+    }
 }
